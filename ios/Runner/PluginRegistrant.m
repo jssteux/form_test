23c032235596fe4a7a05ee6f442dba8b8ce7f826
jssteux <jssteux@osivia.org>
2023-05-17 20:42:39 +0200

    test image ios

---
+++ ios/Runner/PluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "PluginRegistrant.h"
+
+#if __has_include(<image_picker_ios/FLTImagePickerPlugin.h>)
+#import <image_picker_ios/FLTImagePickerPlugin.h>
+#endif
+
+@implementation PluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
+}
+
+@end
