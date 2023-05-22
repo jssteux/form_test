d3c2568185b1c8a255dc4c1020e0b4a8ef6745ff
jssteux <jssteux@osivia.org>
2023-05-22 19:15:48 +0200

    suppression firebase

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,14 +1,11 @@
 import UIKit
 import Flutter
-import Firebase
+
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
 
-  override init() {
-    super.init()
-    FirebaseApp.configure()
-  }
+
 
   override func application(
     _ application: UIApplication,
